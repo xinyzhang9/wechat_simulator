9303b3bfcb396630a39205a309c9cd35b6cecab0
Xinyang Zhang <xinyang.zhang@hpe.com>
2018-04-12 17:02:23 +0800

    organize code in the main and effect functions

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
-xs.periodic(1000)
-.fold(prev => prev + 1, 0)
-.map(i => `second elapsed: ${i}`)
-.subscribe({
-  next: str => {
-    const elem = document.querySelector('#app');
-    elem.textContent = str;
-  }
-})
+function main() {
+  return xs.periodic(1000)
+    .fold(prev => prev + 1, 0)
+    .map(i => `second elapsed: ${i}`);
+}
+
+function domDriver(text$) {
+  text$.subscribe({
+    next: str => {
+      const elem = document.querySelector('#app');
+      elem.textContent = str;
+    }
+  })
+}
+
+function logDriver(msg$) {
+  msg$.subscribe({
+    next: msg => {
+      console.log(msg);
+    }
+  })
+}
+const sink = main();
+domDriver(sink);
+logDriver(sink);
